34fc62229d09b818489e303f9a6ee2d40352b56f
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-13 16:18:31 +0000

    add event listener for mousdown when user hits mole

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ const timeLeft = document.querySelector('#time-left');
 const score = document.querySelector('#score');
 
 let result = 0;
+let hitPosition
 
 function randomSquare() {
     squares.forEach(square => {
@@ -12,11 +13,23 @@ function randomSquare() {
 
     let randomSquare = squares[Math.floor(Math.random() * 9)]; 
     randomSquare.classList.add('mole');
+
+    hitPosition = randomSquare.id
 }
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id === hitPosition){
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
 
 function moveMole() {
     let timerId = null;
-    timerID = setInterval(randomSqaure, 500);
+    timerID = setInterval(randomSquare, 500);
 }
 
 moveMole();
